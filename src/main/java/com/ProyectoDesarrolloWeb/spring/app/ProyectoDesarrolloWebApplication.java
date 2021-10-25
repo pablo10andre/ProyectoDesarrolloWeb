e1cdf4b24ff36d57b55a7fbe6ba7326786dbfdb7
pablo10andre <82068542+pablo10andre@users.noreply.github.com>
2021-10-25 15:06:57 -0600

    Inicio_de_sesion
    configuracion de inicio de sesion y cambio de nombre al metodo controlador de indice

--- src/main/java/com/ProyectoDesarrolloWeb/spring/app/ProyectoDesarrolloWebApplication.java
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/ProyectoDesarrolloWebApplication.java
@@ -1,13 +1,32 @@
 package com.ProyectoDesarrolloWeb.spring.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ProyectoDesarrolloWebApplication {
-
+public class ProyectoDesarrolloWebApplication implements CommandLineRunner {
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoDesarrolloWebApplication.class, args);
 	}
+	@Override
+	public void run(String... args) throws Exception {
+
+		String password = "WilsonCP";
+		
+		for(int i=0; i<2; i++) {
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}
+		
+	}
+	
+	
+	
+	
 
 }
