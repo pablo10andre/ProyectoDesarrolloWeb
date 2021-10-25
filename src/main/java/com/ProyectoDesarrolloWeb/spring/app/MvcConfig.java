6207e82768191ff0e372788698236d9f77dc33b4
pablo10andre <82068542+pablo10andre@users.noreply.github.com>
2021-10-25 11:54:20 -0600

    Spring_security_config
    Configuracion_spring_security

---
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/MvcConfig.java
@@ -0,0 +1,32 @@
+package com.ProyectoDesarrolloWeb.spring.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+
+		registry.addResourceHandler("/uploads/**").addResourceLocations("file:/Users/kc/Desktop/tmp/");
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
