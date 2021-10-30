08b5f4485bfaef8a86604d1116b7096f54931863
pablo10andre <pablo10andre@gmail.com>
2021-10-29 21:14:49 -0600

    SetApisProductPage

--- src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ConsumoApis.java
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ConsumoApis.java
@@ -12,49 +12,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.ProyectoDesarrolloWeb.spring.app.DTO.CambioMoneda;
+import com.ProyectoDesarrolloWeb.spring.app.DTO.USD_GTQ;
 import com.ProyectoDesarrolloWeb.spring.app.DTO.Product;
 
 
 public class ConsumoApis {	
-	public static void main(String[] args) {	
-   /* Product prueba=consumo_producto();
-    System.out.println("titulo: "+prueba.getProductTitle()+"precio: "+prueba.getPrice());
-*/
-		
-		CambioMoneda prueba=consumo_cambio();
-	    System.out.println("titulo: "+prueba.getUSD_GTQ());
-		
-	}
-	
-	public static CambioMoneda consumo_cambio() {
+	public static USD_GTQ consumo_cambio() {
 
 		RestTemplate restTemplate=new RestTemplate();
-		String url="http://free.currconv.com/api/v7/convert";	
+		String url="https://api.exchangerate.host/convert";	
         UriComponentsBuilder uri=UriComponentsBuilder.fromHttpUrl(url)
-        		.queryParam("q","USD_GTQ")
-        		.queryParam("compact","ultra")
-        		.queryParam("apiKey","78cc8ab338b6c20c7d75");
-		
-        HttpHeaders headers=new HttpHeaders();
-        headers.set("apiKey", "78cc8ab338b6c20c7d75");
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Product> entity=new HttpEntity<>(headers);
-        
+        		.queryParam("from","USD")
+        		.queryParam("to","GTQ");
 		System.out.println(uri.toUriString());
 		 
 
 		
-		ResponseEntity<CambioMoneda> responseEntity = 
+		ResponseEntity<USD_GTQ> responseEntity = 
 				  restTemplate.exchange(
 				    uri.toUriString(),
 				    HttpMethod.GET,
-				    entity,
-				    new ParameterizedTypeReference<CambioMoneda>() {}
+				    null,
+				    new ParameterizedTypeReference<USD_GTQ>() {}
 						  );
 		
 		
-		CambioMoneda valor=responseEntity.getBody();
+		USD_GTQ valor=responseEntity.getBody();
 		return valor;
 		
 	}
@@ -65,12 +48,11 @@ public class ConsumoApis {
 	
 	
 	
-	public static Product consumo_producto() {
-
+	public static Product consumo_producto(String urlParam) {
 		RestTemplate restTemplate=new RestTemplate();
 		String url="https://axesso-axesso-amazon-data-service-v1.p.rapidapi.com/amz/amazon-lookup-product";	
         UriComponentsBuilder uri=UriComponentsBuilder.fromHttpUrl(url)
-        		.queryParam("url","https://www.amazon.com/-/es/Corsair-Vengeance-3200MHz-computadora-escritorio/dp/B07D1XCKWW/?_encoding=UTF8&pd_rd_w=RuqTA&pf_rd_p=6e9da02f-f7a3-444f-aea6-9ef09ed8bb89&pf_rd_r=X90BKQB5EMKH9WN1BCX0&pd_rd_r=15daca3e-8343-4ab0-a9c2-e958d3b1be26&pd_rd_wg=pPb9b&ref_=pd_gw_ci_mcx_mr_hp_d");
+        		.queryParam("url",urlParam);
 		
         HttpHeaders headers=new HttpHeaders();
         headers.set("x-rapidapi-host", "axesso-axesso-amazon-data-service-v1.p.rapidapi.com");
---
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ProductController.java
@@ -0,0 +1,29 @@
+package com.ProyectoDesarrolloWeb.spring.app.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ProyectoDesarrolloWeb.spring.app.DTO.Product;
+
+
+
+
+@Controller
+public class ProductController {
+
+	
+	@RequestMapping(value = "/producto")
+	public String editarCliente(@RequestParam(required = false) String url, Model model) {
+		Product producto_api=ConsumoApis.consumo_producto(url);   
+		System.out.println(producto_api.getProductTitle());
+		model.addAttribute("productos",producto_api);
+		return "producto";
+	}	
+	
+	
+}
