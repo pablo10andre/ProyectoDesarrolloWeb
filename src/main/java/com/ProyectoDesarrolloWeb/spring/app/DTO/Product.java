b9be3ff247334e95fb1c2035b56f3c41c7db01ef
pablo10andre <pablo10andre@gmail.com>
2021-10-30 21:30:04 -0600

    set_product_pageProducto

--- src/main/java/com/ProyectoDesarrolloWeb/spring/app/DTO/Product.java
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/DTO/Product.java
@@ -1,14 +1,29 @@
 package com.ProyectoDesarrolloWeb.spring.app.DTO;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties
 public class Product {
 String productTitle;
 BigDecimal price;
+List<String> imageUrlList;
+List<String> features;
 
 
+public List<String> getFeatures() {
+	return features;
+}
+public void setFeatures(List<String> features) {
+	this.features = features;
+}
+public List<String> getImageUrlList() {
+	return imageUrlList;
+}
+public void setImageUrlList(List<String> imageUrlList) {
+	this.imageUrlList = imageUrlList;
+}
 public String getProductTitle() {
 	return productTitle;
 }
--- src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ProductController.java
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ProductController.java
@@ -3,9 +3,7 @@ package com.ProyectoDesarrolloWeb.spring.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ProyectoDesarrolloWeb.spring.app.DTO.Product;
@@ -20,7 +18,6 @@ public class ProductController {
 	@RequestMapping(value = "/producto")
 	public String producto(@RequestParam(required = true) String url, Model model) {
 		Product producto_api=ConsumoApis.consumo_producto(url);   
-		System.out.println(producto_api.getProductTitle());
 		model.addAttribute("productos",producto_api);
 		return "producto";
 	}	
