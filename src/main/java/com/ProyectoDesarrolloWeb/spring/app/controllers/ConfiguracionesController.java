c2733468c0b101dbc4808910dfb3f21c193a02a8
pablo10andre <pablo10andre@gmail.com>
2021-10-30 22:20:10 -0600

    Configuraciones_controller

---
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/controllers/ConfiguracionesController.java
@@ -0,0 +1,25 @@
+package com.ProyectoDesarrolloWeb.spring.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ProyectoDesarrolloWeb.spring.app.entities.Configuraciones;
+import com.ProyectoDesarrolloWeb.spring.app.repositories.ConfiguracionesRepository;
+
+
+@Controller
+public class ConfiguracionesController {
+	@Autowired
+	private ConfiguracionesRepository configuraciones;
+
+	@RequestMapping(value = "/configuraciones", method = RequestMethod.GET)
+	public String detalleCliente(Model model) {
+
+		Configuraciones configuracion = configuraciones.findById((long) 1).get();
+		model.addAttribute("configuracion", configuracion);
+		return "configuracion";
+	}
+}
