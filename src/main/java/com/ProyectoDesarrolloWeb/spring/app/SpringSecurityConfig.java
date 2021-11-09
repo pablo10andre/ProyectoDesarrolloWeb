da1d1f21937fecf7579f1990776fd5a9d06973a5
Wcano74 <66225081+Wcano74@users.noreply.github.com>
2021-11-08 20:09:29 -0600

    pagina_producto
    diseño de la pagina producto funcion de los datos generados del llamado de la API

--- src/main/java/com/ProyectoDesarrolloWeb/spring/app/SpringSecurityConfig.java
+++ src/main/java/com/ProyectoDesarrolloWeb/spring/app/SpringSecurityConfig.java
@@ -28,7 +28,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.authorizeRequests().antMatchers("/", "/css/**","/scss/**", "/js/**", "/images/**", "/producto","/index").permitAll()
+		http.authorizeRequests().antMatchers("/", "/css/**","/scss/**", "/js/**", "/img/**", "/producto","/index").permitAll()
 
 		.anyRequest().authenticated()
 		.and()
